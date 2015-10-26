3a6be376d1308307bd2659988c9fc9c3719d180e
Daniel Carriola <daniel.carriola@gmail.com>
2015-10-26 17:53:00 +0100

    merge_swarms

--- nmmso/R/nmmso_iterative.R
+++ nmmso/R/nmmso_iterative.R
@@ -1,3 +1,5 @@
+library(flexclust)
+
 NMMSO_iterative(
   swarm_size, problem_function, problem_function_params, max_evaluations, mn, mx, evaluations, nmmso_state, max_evol, tol_val
 ) {
@@ -92,7 +94,117 @@ evaluate_first(swarm, problem_function, problem_function_params, nmmso_state, sw
 }
 
 merge_swarms <- function(nmmso_state, problem_function, problem_function_params, mn, mx){
+  I = which(nmmso_state$active_modes_changed == 1)
+  nmmso_state$active_modes_changed = nmmso_state$active_modes_changed * 0 # reset
   
+  n = length(I)
+  number_of_mid_evals = 0
+  if(n >= 1 && (length(nmmso_state$active_modes) > 1)) {
+    to_compare = matrix(0, 2)
+    to_compare[, 1] = I
+    for(i in 1:n) {
+      d = dist2(nmmso_state$M_loc[I[i], ], nmmso_state$M_loc)
+      d[I[i]] = Inf
+      tmp = which(to_compare[i, 2] == min(to_compare[i, 2]))
+      nmmso_state$active_modes[I[i]]$swarm$dist = sqrt(tmp)
+      
+      if(nmmso_state$active_modes[I[i]]$swarm$number_of_particles == 1) {
+        reject = 0
+        temp_vel = mn - 1
+        while(sum(temp_vel < mn) > 0 || sum(temp_vel > mx) > 0) {
+          temp_vel = uniform_sphere_points(1, length(nmmso_state$active_modes[I[i]]$swarm$new_location)) 
+                   * (nmmso_state$active_modes[I[i]]$swarm$dist / 2)
+          reject = reject + 1
+          if(reject > 20) {
+            size = dim(nmmso_state$active_modes[I[i]]$swarm$new_location)
+            temp_vel = matrix(runif(size, min = (mx - mn), max = mn), size)
+          }
+        }
+        nmmso_state$active_modes[I[i]]$swarm$velocities[1, ] = temp_vel
+      }
+    }
+    
+    to_compare = t(apply(to_compare, 1, sort))
+    to_compare = apply(to_compare, 2, sort)
+    
+    # Remove duplicates
+    for(i in seq(n, -1, 2)) {
+      I = which(to_compare[, 1] == to_compare[i, 1])
+      repeat_matrix = kronecker(matrix(1, length(I), 1, to_compare[i, ]))
+      if (# TODO: check the repeat_matrix with Matlab) {
+        to_compare[i, ] = matrix()
+      }
+    }
+    
+    # Check for merging
+    n = length(to_compare)
+    to_merge = matrix()
+    number_of_mid_evals = 0
+    
+    for(i in 1:n) {
+      if(sqrt(dist2(nmmso_state$active_modes(to_compare[i, 1])$swarm$mode_location, nmmso_state$active_modes[to_compare[i, 2]]$swarm$mode_location)) 
+         < nmmso_state$tol_val) {
+        to_merge = matrix(to_merge, i)
+      } else {
+        mid_loc = 0.5 
+              * (nmmso_state$active_modes[to_compare[i, 1]]$swarm$mode_location - nmmso_state$active_modes[to_compare[i, 2]]$swarm$mode_location)
+              + nmmso_state$active_modes[to_compare[i, 2]]$swarm$mode_location
+        
+        if(sum(mid_loc < mn) > 0 || sum(mid_loc > mx) > 0) {
+          stop('Mid point out of range!')
+        }
+        
+        nmmso_state$active_modes[to_compare[i, 2]]$swarm$new_location = mid_loc
+        #evaluate_mid
+        
+        if(mode_shift == 1) {
+          nmmso_state$M_loc[to_compare[i, 2], ] = nmmso_state$active_modes[to_compare[i, 2]]$swarm$mode_location
+          nmmso_state$V_loc[to_compare[i, 2]] = nmmso_state$active_modes[to_compare[i, 2]]$swarm$mode_value
+          to_merge = matrix(to_merge, i)
+          nmmso_state$active_modes_changed[to_compare[i, 2]] = 1
+        } else if(nmmso_state$active_modes[to_compare[i, 2]]$swarm$mode_value < y) {
+          to_merge = matrix(to_merge, i) 
+        }
+        
+        number_of_mid_evals = number_of_mid_evals + 1
+      }
+    }
+    
+    delete_index = matrix(0, dim(to_merge))
+    for(i in 1:length(to_merge)) {
+      if(to_compare[to_merge[i], 2] == to_compare[to_merge[i], 1]) {
+        stop('Indices should not be equal')
+      }
+      if(nmmso_state$active_modes[to_compare[to_merge[i], 1]]$swarm$mode_value > nmmso_state$active_modes[to_compare[to_merge[i], 2]]$swarm$mode_value) {
+        delete_index[i] = to_compare[to_merge[i], 2]
+        nmmso_state$active_modes[to_compare[to_merge[i], 1]]$swarm = merge_swarms_together(nmmso_state$active_modes[to_compare[to_merge[i], 1]]$swarm, 
+                                                                                           nmmso_state$active_modes[to_compare[to_merge[i], 2]]$swarm)
+        nmmso_state$active_modes_changed[to_compare[i, 1]] = 1
+      } else {
+        delete_index[i] = to_compare[to_merge[i], 1]
+        nmmso_state$active_modes[to_compare[to_merge[i], 2]]$swarm = merge_swarms_together(nmmso_state$active_modes[to_compare[to_merge[i], 2]]$swarm,
+                                                                                           nmmso_state$active_modes[to_compare[to_merge[i], 1]]$swarm)
+        nmmso_state$active_modes_changed[to_compare[i, 2]] = 1
+      }
+    }
+    
+    prev_merge = -1
+    delete_index = apply(delete_index, 2, sort)
+    for(i in seq(length(delete_index), -1, 1)) {
+      if(delete_index[i] != prev_merge) {
+        prev_merge = delete_index[i]
+        nmmso_state$active_modes[delete_index[i]] = matrix()
+        nmmso_state$M_loc[delete_index[i], ] = matrix()
+        nmmso_state$V_loc[delete_index[i]] = matrix()
+        nmmso_state$converged_modes[delete_index[i]] = matrix()
+        nmmso_state$active_modes_changed[delete_index[i]] = matrix()
+      }
+    }
+  } 
+  
+  if(length(nmmso_state$active_modes) == 1) {
+    nmmso_state$active_modes[1]$swarm$dist = apply(mx - mn, 2, min)
+  }
 }
 
 evaluate <- function(nmmso_state, chg, problem_function, problem_function_params){
