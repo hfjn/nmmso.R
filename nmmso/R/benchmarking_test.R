4131dca1eb8617c1f679401aaaa0b111ebe59b30
Jannik Hoffjann <jhoffjann@gmail.com>
2015-11-02 16:29:26 +0100

    started to implement the benchmarking functions

---
+++ nmmso/R/benchmarking_test.R
@@ -0,0 +1,12 @@
+set.seed = 20151102
+
+# still missing
+cec_2015_problem_data
+
+# using the CEC test problems
+problem_used = niching_func
+
+eval_list = list()
+c_list = list()
+pop_size = list()
+initial_flag;
