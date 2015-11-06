8647a309a1a8fb57e961565cae02d46c463fedd3
Daniel Carriola <daniel.carriola@gmail.com>
2015-11-06 20:17:00 +0100

    Forgot this function

--- R/feval.R
+++ R/feval.R
@@ -1,6 +1,10 @@
-#' @title 
-#' @param 
-# helper functions which imitates the behavior of the Matlab feval
+#' @title feval
+#' @description Helper functions which imitates the behavior of the Matlab feval.
+#'
+#' @param f Function given to be run.
+#' @param ... Passing arguments on to other functions.
+#'
+#' @export
 feval <- function(f,...) {
   f(...)
 }
