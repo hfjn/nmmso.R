507ca90a0e44ff9df326e6592cc096418a1e118f
Daniel Carriola <daniel.carriola@gmail.com>
2015-11-09 14:26:52 +0100

    Final documentation of add_row

--- R/add_row.R
+++ R/add_row.R
@@ -3,9 +3,9 @@
 #' It works simulating the behavior of a matrix in Matlab. 
 #' Everytime you want to add a row or a full matrix in an out of bounds index, automatically is created with the row given.
 #' 
-#' @param original Object with 
-#' @param row_index String containing name of function to be optimised.
-#' @param new_object Meta-parameters needed by problem function.
+#' @param original Initial variable where the final matrix will be stored, it can be already matrix.
+#' @param row_index Numeric index representing the row where the new line should be inserted.
+#' @param new_object Row or matrix to be inserted in the original one.
 #' @return The matrix with the changes done after the function was run.
 #' 
 #' @export
