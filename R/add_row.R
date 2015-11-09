f72551481670fa496fd7567e85a97b4eb23d7388
Jannik Hoffjann <jhoffjann@gmail.com>
2015-11-09 14:11:02 +0100

    fixed small error in add_row

--- R/add_row.R
+++ R/add_row.R
@@ -1,8 +1,20 @@
 add_row <- function(x, n, a){
-	if(n != 2){
-		while(nrow(x) < n - 1 || is.null(nrow(x))){
-			x <- rbind(x, 0)
+	if(!is.null(nrow(x))) {
+		if(n <= nrow(x)) {
+			x[n, ] <- a
+		}
+		else if(n >= 2) {
+			while(nrow(x) < n - 1) {
+				x <- rbind(x, 0)
+			}
+			x <- rbind(x, a)
 		}
 	}
-	rbind(x, a)
+	else if(n == 2){
+		x <- rbind(x, a)
+	}	
+	else{
+		x <- a
+	}
+	return(x);	
 }
