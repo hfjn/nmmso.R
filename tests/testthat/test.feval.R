71b5a08d6c6c547f87729b235e8e5772b61015c7
Jannik Hoffjann <jhoffjann@gmail.com>
2015-12-04 21:00:26 +0100

    added test

--- tests/testthat/test.feval.R
+++ tests/testthat/test.feval.R
@@ -1,4 +1,4 @@
-contex("feval")
+context("feval")
 test_that("feval is working",{
   expect_equal(sqrt(10), feval(sqrt, 10))
 })
--- tests/testthat/test.new_dist2.R
+++ tests/testthat/test.new_dist2.R
@@ -1,17 +1,10 @@
 context("new_dist2")
 test_that("new_dist2", {
-  x = rbind(c(1, 2, 3), 
-            c(4, 5, 6),
-            c(7, 8 ,9))
+  x = matrix(c(1, 2, 3, 4, 5, 6, 7, 8 ,9), 3)
+  c = matrix(c(9, 8, 7, 6, 5, 4, 3, 2, 1), 3)
 
-  c = rbind(c(9, 8, 7), 
-            c(6, 5, 4), 
-            c(3, 2, 1))
+  r = matrix(c(84, 75, 72, 75, 72, 75, 72, 75, 84), 3)
+  expect_true(all.equal(new_dist2(x, c), r))
 
-  r = rbind(c(116, 35,  8),
-            c(35, 8,  35),
-            c(8, 35, 116))
-
-  expect_true(new_dist2(x, c) == r)
 
 })
